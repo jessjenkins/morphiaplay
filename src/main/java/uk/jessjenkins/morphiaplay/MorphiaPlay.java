79b166730286355f34cf0a6a885d8d0bba83b357
Jessica Jenkins <jessica.jenkins@dvla.gsi.gov.uk>
2018-02-13 13:20:39 +0000

    Create DB, add two records and drop.

--- src/main/java/uk/jessjenkins/morphiaplay/MorphiaPlay.java
+++ src/main/java/uk/jessjenkins/morphiaplay/MorphiaPlay.java
@@ -1,4 +1,32 @@
 package uk.jessjenkins.morphiaplay;
 
+import com.mongodb.MongoClient;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+import uk.jessjenkins.morphiaplay.entities.Record;
+
+import java.util.UUID;
+
 public class MorphiaPlay {
+    public static void main(String[] args) {
+        System.out.println("Playing with morphia…");
+
+        Morphia morphia = new Morphia();
+
+        morphia.mapPackage(Record.class.getPackage().getName());
+
+        final Datastore datastore = morphia.createDatastore(
+                new MongoClient(),"morphiaplay");
+        datastore.ensureIndexes();
+
+
+        final Record r1 = Record.builder().recordId(UUID.randomUUID()).name("Jess").build();
+        final Record r2 = Record.builder().recordId(UUID.randomUUID()).name("Fred").build();
+
+        datastore.save(r1);
+        datastore.save(r2);
+
+        datastore.getCollection(Record.class).drop();
+        datastore.getDB().dropDatabase();
+    }
 }
--- src/main/java/uk/jessjenkins/morphiaplay/entities/Record.java
+++ src/main/java/uk/jessjenkins/morphiaplay/entities/Record.java
@@ -2,12 +2,14 @@ package uk.jessjenkins.morphiaplay.entities;
 
 import lombok.Builder;
 import lombok.Data;
+import org.mongodb.morphia.annotations.Entity;
 import org.mongodb.morphia.annotations.Id;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@Entity("records")
 public class Record {
     @Id
     private UUID recordId;
